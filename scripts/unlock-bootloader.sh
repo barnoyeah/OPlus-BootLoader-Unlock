c273393f690dc56e70fc346e71e45f3c500915a5
barnoyeah <alpaymlh1@gmail.com>
2026-07-15 14:06:20 +0300

    Update unlock-bootloader.sh
    actually fixes the damn thing

--- scripts/unlock-bootloader.sh
+++ scripts/unlock-bootloader.sh
@@ -1,20 +1,21 @@
-@echo off
-REM Universal Bootloader Unlock Script for OPPO & OnePlus (This file is for macOS users and Linux ones)
+#!/usr/bin/bash
+
+# @echo off
+# REM Universal Bootloader Unlock Script for OPPO & OnePlus (This file is for macOS users and Linux ones)
 
 echo === Bootloader Unlock Script
 
-:: Step 1: Reboot into fastboot mode
-echo [*] Rebooting into fastboot mode...
-adb reboot bootloader
-
-:: Step 2: Check device in fastboot
-echo [*] Checking fastboot device...
+echo "[*] rebooting device into fastboot."
+adb reboot bootloader 
+echo "[*] Checking fastboot device..."
 fastboot devices
 
-:: Step 3: Try unlock command
-echo [*] Attempting to unlock...
+echo "[*] Attempting to unlock..."
 fastboot flashing unlock || fastboot oem unlock
 
-echo [*] If your device asks for confirmation, use volume keys + power button.
-echo Done! Your bootloader should now be unlocked.
-pause
+echo "[ TROUBLESHOOTING ]"
+
+echo "1- [*] If your device asks for confirmation, use volume keys + power button."
+echo "2- [*] Done! Your bootloader should now be unlocked."
+echo "3- [!] If your device reboots into original android then try (adb reboot bootloader)."
+echo "For more information please check the docs."
