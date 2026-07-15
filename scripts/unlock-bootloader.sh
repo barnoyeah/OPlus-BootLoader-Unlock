b3136b8f0e7712c4deb2a81d88ef8ff7953c55f7
barnoyeah <alpaymlh1@gmail.com>
2026-07-15 14:08:02 +0300

    Update unlock-bootloader.sh

--- scripts/unlock-bootloader.sh
+++ scripts/unlock-bootloader.sh
@@ -5,17 +5,18 @@
 
 echo === Bootloader Unlock Script
 
-echo "[*] rebooting device into fastboot."
+echo "[*] Rebooting device into fastboot."
 adb reboot bootloader 
 echo "[*] Checking fastboot device..."
 fastboot devices
 
-echo "[*] Attempting to unlock..."
+echo "[*] Attempting to unlock..." # oh boy
 fastboot flashing unlock || fastboot oem unlock
+
+echo "[✓] Congrats! your bootloader should probably be unlocked."
 
 echo "[ TROUBLESHOOTING ]"
 
 echo "1- [*] If your device asks for confirmation, use volume keys + power button."
-echo "2- [*] Done! Your bootloader should now be unlocked."
-echo "3- [!] If your device reboots into original android then try (adb reboot bootloader)."
+echo "2- [*] If your device reboots into original android then try (adb reboot bootloader)."
 echo "For more information please check the docs."
